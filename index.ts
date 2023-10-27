581181a4d3f6512ffc921458d821b15f7b0bb1e1
bangdpc <bangdanh105@gmail.com>
2023-10-27 18:05:51 +0700

    done: remove two data

---
+++ index.ts
@@ -0,0 +1,13 @@
+function removeElement(nums: number[], val: number):void {
+    const newNums:number[] =[]
+    for(let i = 0; i< nums.length - 1; i++)
+    {
+        if(nums[i] !== val) {
+            newNums.push(nums[i]);
+        }
+    }
+    console.log(newNums.length)
+};
+const nums1:number[] = [0,1,2,2,3,0,4,2]
+const val1:number=2
+const data =removeElement(nums1,val1)
